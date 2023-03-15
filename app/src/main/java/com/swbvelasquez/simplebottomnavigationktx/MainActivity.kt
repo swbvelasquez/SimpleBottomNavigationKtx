bb9e5f468d723408adfc9f99158cfb0a28f505db
swbvelasquez <swbvelasquez@gmail.com>
2023-03-14 21:46:57 -0500

    Agregado fragmentos

--- app/src/main/java/com/swbvelasquez/simplebottomnavigationktx/MainActivity.kt
+++ app/src/main/java/com/swbvelasquez/simplebottomnavigationktx/MainActivity.kt
@@ -2,7 +2,10 @@ package com.swbvelasquez.simplebottomnavigationktx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.core.view.get
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
 import com.swbvelasquez.simplebottomnavigationktx.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +15,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bnvMain.background = null
+        binding.bnvMain.background = null //Permite quitar el background del bottom nav view
+        binding.bnvMain.setOnItemSelectedListener { menuItem ->
+            val fragment:Fragment? = when(menuItem.itemId){
+                R.id.optHome -> HomeFragment()
+                R.id.optFavorite -> FavoriteFragment()
+                R.id.optProfile -> ProfileFragment()
+                R.id.optHistory -> HistoryFragment()
+                else -> null
+            }
+
+            if(fragment!=null){
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.lyContentMain,fragment)
+                    .commit()
+            }
+
+            true
+        }
     }
 }
